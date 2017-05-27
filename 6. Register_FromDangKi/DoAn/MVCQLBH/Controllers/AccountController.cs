7883b8a2ce5e22d738649f375aaa4b03d8d7ef7a
TuHuynh <tuhuynhkhtn@gmail.com>
2017-05-27 19:42:46 +0700

    Register_Form Đăng kí (dùng validation)
    Register_Form Đăng kí (dùng validation)

---
+++ 6. Register_FromDangKi/DoAn/MVCQLBH/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using MVCQLBH.Models;
+using MVCQLBH.Ultilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCQLBH.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserRegisting user)
+        {
+            var u = new User
+            {
+                f_Username = user.Username,
+                f_Password = Ulti.Md5Hash(user.Password),
+                f_Name = user.Name,
+                f_Email = user.Email,
+                f_DOB = DateTime.ParseExact(user.DOB, "d/m/yyyy", null)
+            };
+
+            using (var dc = new QLBHEntities())
+            {
+                dc.Users.Add(u);
+                dc.SaveChanges();
+            }
+
+            return View();
+        }
+    }
+}
