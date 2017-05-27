531caa84545432ad452f3b8c68bcf661de31b6cd
TuHuynh <tuhuynhkhtn@gmail.com>
2017-05-27 20:24:01 +0700

    AddCartItemByCat_Thêm hàng vào giỏ qua Category
    AddCartItemByCat_Thêm hàng vào giỏ qua Category

---
+++ 16. AddCartItemByCat/DoAn/MVCQLBH/Controllers/CartController.cs
@@ -0,0 +1,56 @@
+﻿using MVCQLBH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCQLBH.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Detail()
+        {
+            var cart = Session["cart"] as Cart;
+            return View(cart);
+        }
+
+        // POST: Cart/Add
+        [HttpPost]
+        public ActionResult AddCIFomDetail(int proId, int quantity)
+        {
+            if (Session["cart"] == null)
+            {
+                Session["cart"] = new Cart();
+            }
+            var c = Session["cart"] as Cart;
+            c.AddItem(proId, quantity);
+
+            //using (var dc = new QLBHEntities())
+            //{
+            //    var pro = dc.Products.Where(p => p.ProID == proId).FirstOrDefault();
+            //    if(pro != null)
+            //    {
+            //        c.Items.Add(new CartItem { Product = pro, Quantity = quantity });
+            //    }
+            //}
+
+            return RedirectToAction("Detail", "Product", new { id = proId });
+        }
+
+        // POST: Cart/Add
+        [HttpPost]
+        public ActionResult AddCIFromListProduct(int proId, int catId, int page)
+        {
+            if (Session["cart"] == null)
+            {
+                Session["cart"] = new Cart();
+            }
+            var c = Session["cart"] as Cart;
+            c.AddItem(proId);
+            return RedirectToAction("GetListByCategory", "Product", new { id = catId, page = page });
+        }
+
+    }
+}
