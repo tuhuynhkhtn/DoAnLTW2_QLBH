a43212193548c07d43081ffb6039571727fd34c3
TuHuynh <tuhuynhkhtn@gmail.com>
2017-05-27 20:18:44 +0700

    Cart_AddItem
    Cart_AddItem

---
+++ 14. Cart_AddItem/DoAn/MVCQLBH/Controllers/CartController.cs
@@ -0,0 +1,38 @@
+﻿using MVCQLBH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCQLBH.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Cart/Add
+        [HttpPost]
+        public ActionResult Add(int proId, int quantity)
+        {
+            if(Session["cart"] == null)
+            {
+                Session["cart"] = new Cart();
+            }
+            var c = Session["cart"] as Cart;
+            using (var dc = new QLBHEntities())
+            {
+                var pro = dc.Products.Where(p => p.ProID == proId).FirstOrDefault();
+                if(pro != null)
+                {
+                    c.Items.Add(new CartItem { Product = pro, Quantity = quantity });
+                }
+            }
+            return RedirectToAction("Detail", "Product", new { id = proId });
+        }
+    }
+}
---
+++ 14. Cart_AddItem/DoAn/MVCQLBH/Models/Cart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCQLBH.Models
+{
+    public class Cart
+    {
+        public IList<CartItem> Items { get; }
+        public Cart()
+        {
+            Items = new List<CartItem>();
+        }
+
+        public int TotalQuantity()
+        {
+            return Items.Sum(i => i.Quantity);
+        }
+    }
+
+    public class CartItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
