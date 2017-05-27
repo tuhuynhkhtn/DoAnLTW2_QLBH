accba705d64495a29b9d5001ab33797599e936e1
TuHuynh <tuhuynhkhtn@gmail.com>
2017-05-28 00:10:11 +0700

    Hiển thị 10 sản phẩm theo yêu cầu ở Trang chủ
    Hiển thị 10 sản phẩm theo yêu cầu ở Trang chủ

---
+++ 22.8 HienThiSanPhamChoTrangChu/DoAn/MVCQLBH/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using MVCQLBH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCQLBH.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home
+        public ActionResult HienThiSPMoiNhat()
+        {
+            using (var dc = new QLBHEntities())
+            {
+                var l = dc.Products
+                    .Where(p => p.BiXoa == 0)
+                    .OrderBy(p => p.NgayNhap)
+                    .OrderByDescending(p => p.NgayNhap)
+                    .Take(10)
+                    .ToList();
+                return PartialView("_PartialSPMoiNhat", l);
+            }
+        }
+
+        // GET: Home
+        public ActionResult HienThiSPBanChayNhat()
+        {
+            using (var dc = new QLBHEntities())
+            {
+                var l = dc.Products
+                    .Where(p => p.BiXoa == 0)
+                    .OrderBy(p => p.SoLuongDaBan)
+                    .OrderByDescending(p => p.SoLuongDaBan)
+                    .Take(10)
+                    .ToList();
+                return PartialView("_PartialSPBanChayNhat", l);
+            }
+        }
+
+        // GET: Home
+        public ActionResult HienThiSPDuocXemNhieuNhat()
+        {
+            using (var dc = new QLBHEntities())
+            {
+                var l = dc.Products
+                    .Where(p => p.BiXoa == 0)
+                    .OrderBy(p => p.SoLuotXem)
+                    .OrderByDescending(p => p.SoLuotXem)
+                    .Take(10)
+                    .ToList();
+                return PartialView("_PartialSPDuocXemNhieuNhat", l);
+            }
+        }
+    }
+}
