cf5e3fa097a934a4f25184f1ef07e6098650ebbc
TuHuynh <tuhuynhkhtn@gmail.com>
2017-05-27 20:44:09 +0700

    Thêm sản phẩm (Admin)
    Thêm sản phẩm (Admin)

---
+++ 21. AddProduct/DoAn/MVCQLBH/Controllers/ManageProductController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCQLBH.Models;
+using MVCQLBH.Ultilities;
+
+namespace MVCQLBH.Controllers
+{
+    [AuthActionFilter(RequiredPermission = 1)]
+
+    public class ManageProductController : Controller
+    {
+        // GET: ManageProduct
+        public ActionResult IndexManageProduct()
+        {
+            using (var dc = new QLBHEntities())
+            {
+                var l = dc.Products.ToList();
+                return View(l);
+            }
+        }
+
+        // GET: ManageProduct/Delete
+        public ActionResult Delete(int id)
+        {
+            using (var dc = new QLBHEntities())
+            {
+                var pD = dc.Products.Where(p => p.ProID == id).FirstOrDefault();
+                if(pD != null)
+                {
+                    dc.Products.Remove(pD);
+                    dc.SaveChanges();
+                }
+                Ulti.DeleteProductImgs(id, Server.MapPath("~"));
+                return RedirectToAction("IndexManageProduct");
+            }
+        }
+
+        // GET: ManageProduct/Add
+        public ActionResult Add()
+        {
+            var p = new Product
+            {
+                ProName = "product",
+                CatID = 3,
+                Price = 100000,
+                Quantity = 2
+            };
+            return View(p);
+        }
+
+        // POST: ManageProduct/Add
+        [HttpPost]
+        public ActionResult Add(Product p, HttpPostedFileBase imgLg, HttpPostedFileBase imgSm)
+        {
+            if(p.TinyDes == null)
+            {
+                p.TinyDes = string.Empty;
+            }
+            if (p.FullDes == null)
+            {
+                p.FullDes = string.Empty;
+            }
+            using (var dc = new QLBHEntities())
+            {
+                dc.Products.Add(p);
+                dc.SaveChanges();
+            }
+            Ulti.SaveProductImgs(p.ProID, Server.MapPath("~"), imgLg, imgSm);
+            return RedirectToAction("IndexManageProduct");
+        }
+
+    }
+}
