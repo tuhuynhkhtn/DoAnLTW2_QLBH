39c0eec14d5fa5da4654caee49d86a615134ee60
TuHuynh <tuhuynhkhtn@gmail.com>
2017-05-27 20:20:47 +0700

    CartDetail_Chi tiết giỏ hàng
    CartDetail_Chi tiết giỏ hàng

---
+++ 15. CartDetail/DoAn/MVCQLBH/Controllers/CartController.cs
@@ -0,0 +1,42 @@
+﻿using MVCQLBH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCQLBH.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Detail()
+        {
+            var cart = Session["cart"] as Cart;
+            return View(cart);
+        }
+
+        // POST: Cart/Add
+        [HttpPost]
+        public ActionResult Add(int proId, int quantity)
+        {
+            if(Session["cart"] == null)
+            {
+                Session["cart"] = new Cart();
+            }
+            var c = Session["cart"] as Cart;
+            c.AddItem(proId, quantity);
+
+            //using (var dc = new QLBHEntities())
+            //{
+            //    var pro = dc.Products.Where(p => p.ProID == proId).FirstOrDefault();
+            //    if(pro != null)
+            //    {
+            //        c.Items.Add(new CartItem { Product = pro, Quantity = quantity });
+            //    }
+            //}
+
+            return RedirectToAction("Detail", "Product", new { id = proId });
+        }
+    }
+}
