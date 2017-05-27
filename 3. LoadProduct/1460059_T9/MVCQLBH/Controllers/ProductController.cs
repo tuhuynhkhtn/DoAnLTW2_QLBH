a748a2f0c7df2a85087d1d1a2c016eb1fe507bbe
TuHuynh <tuhuynhkhtn@gmail.com>
2017-05-27 19:18:02 +0700

    Load Product của Category
    Load Product của Category

---
+++ 3. LoadProduct/1460059_T9/MVCQLBH/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using MVCQLBH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCQLBH.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult GetListByCategory(int catId)
+        {
+            using (var dc = new QLBHEntities())
+            {
+                var l = dc.Products
+                    .Where(p => p.CatID == catId)
+                    .ToList();
+                return View("ListByCategory", l);
+            }
+
+        }
+    }
+}
