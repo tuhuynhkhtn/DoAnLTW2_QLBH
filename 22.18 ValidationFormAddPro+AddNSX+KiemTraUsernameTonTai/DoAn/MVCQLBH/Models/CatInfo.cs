d6a1b0f3ae746b43ec02ac5a8a7ca8961a4366b1
TuHuynh <tuhuynhkhtn@gmail.com>
2017-06-02 19:51:52 +0700

    ValidationFormAddPro+AddNSX+Kiểm tra Username tồn tại
    ValidationFormAddPro+AddNSX+Kiểm tra Username tồn tại

---
+++ 22.18 ValidationFormAddPro+AddNSX+KiemTraUsernameTonTai/DoAn/MVCQLBH/Models/CatInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCQLBH.Models
+{
+    public class CatInfo
+    {
+        public string CatNameInfo { get; set; }
+    }
+}
