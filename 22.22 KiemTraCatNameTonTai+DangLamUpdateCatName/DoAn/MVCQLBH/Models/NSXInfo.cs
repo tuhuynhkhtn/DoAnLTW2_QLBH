d16ae42add2695024c4ee4848bb2c4a80dcebe30
TuHuynh <tuhuynhkhtn@gmail.com>
2017-06-03 21:04:41 +0700

    Kiểm tra CatName tồn tại + Đang làm Update CatName
    Kiểm tra CatName tồn tại + Đang làm Update CatName

---
+++ 22.22 KiemTraCatNameTonTai+DangLamUpdateCatName/DoAn/MVCQLBH/Models/NSXInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCQLBH.Models
+{
+    public class NSXInfo
+    {
+        public string NSXNameInfo { get; set; }
+    }
+}
