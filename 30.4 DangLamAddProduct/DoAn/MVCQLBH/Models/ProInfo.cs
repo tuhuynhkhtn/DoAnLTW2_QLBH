48f72efa0a1c4d9cbed435fda7b967f4bfb19fbd
TuHuynh <tuhuynhkhtn@gmail.com>
2017-06-08 11:35:14 +0700

    Đang làm Add Product
    Đang làm Add Product

---
+++ 30.4 DangLamAddProduct/DoAn/MVCQLBH/Models/ProInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCQLBH.Models
+{
+    public class ProInfo
+    {
+        public int ProIDInfo { get; set; }
+        public string ProNameInfo { get; set; }
+        public string TinyDesInfo { get; set; }
+
+        //public string FullDesInfo { get; set; }
+        [AllowHtml]
+        public string FullDesRaw { get; set; }
+
+        public decimal PriceInfo { get; set; }
+        public int CatIDInfo { get; set; }
+        public int QuantityInfo { get; set; }
+        public string NgayNhapInfo { get; set; }
+        public int SoLuotXemInfo { get; set; }
+        public string XuatXuInfo { get; set; }
+        public int LoaiInfo { get; set; }
+        public int IDNhaSanXuatInfo { get; set; }
+        public byte BiXoaInfo { get; set; }
+        public int SoLuongDaBanInfo { get; set; }
+
+        public HttpPostedFileBase imgLg { get; set; }
+        public HttpPostedFileBase imgSm { get; set; }
+
+    }
+}
